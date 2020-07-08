a34f45e9e36ae4481e3251d4bd80fc6ad9be4742
Maricaya <915270549@qq.com>
2020-07-08 16:51:57 +0800

    添加 onClick 事件

--- lib/Icon.tsx
+++ lib/Icon.tsx
@@ -3,14 +3,15 @@ import './importIcons';
 import './icon.scss';
 
 type IconProps = {
-    name: string
+    name: string;
+    onClick: () => void
 }
 
-const Icon: React.FunctionComponent<IconProps> = (prop) => {
+const Icon: React.FunctionComponent<IconProps> = (props) => {
     return (
     <span>
-        <svg className="xue-icon">
-            <use xlinkHref={`#${prop.name}`}/>
+        <svg className="xue-icon" onClick={props.onClick}>
+            <use xlinkHref={`#${props.name}`}/>
         </svg>
     </span>);
 };
