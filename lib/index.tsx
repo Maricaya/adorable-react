38057111a3cb503e58045e2c23144e49369d29e8
Maricaya <915270549@qq.com>
2020-07-08 17:26:54 +0800

    支持传 className

--- lib/index.tsx
+++ lib/index.tsx
@@ -2,8 +2,14 @@ import ReactDOM from 'react-dom';
 import * as React from 'react';
 import Icon from './Icon';
 
+const fn = (e: React.MouseEvent<SVGElement | SVGUseElement>) => {
+    console.log(e.target);
+    console.log((e.target as SVGUseElement).x)
+};
+
 ReactDOM.render(<div>
-    <Icon name="wechat" onClick={() => {
-        console.log(111)
-    }}/>
+    <Icon name="wechat"
+          onClick={fn}
+          onMouseEnter={fn}
+    />
 </div>, document.getElementById('root'));
