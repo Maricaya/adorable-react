dffac88724595bc9bf32ce488322813a5b9679b9
Maricaya <915270549@qq.com>
2020-07-08 15:47:36 +0800

    自动加载所有 icons/*.svg 文件

--- lib/Icon.tsx
+++ lib/Icon.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
+import './importIcons';
 
 type IconProps = {
     name: string
 }
 
 const Icon: React.FunctionComponent<IconProps> = (prop) => {
-    return (<>
-        {prop.name}
-        <span>icon</span>
-    </>)
+    return (
+    <span>
+        <svg>
+            <use xlinkHref={`#${prop.name}`}/>
+        </svg>
+    </span>);
 };
 export default Icon;
 
