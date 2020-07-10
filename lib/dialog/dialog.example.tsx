7abdf7e9fef0a577930982533d065e2f9cbce63d
Maricaya <915270549@qq.com>
2020-07-10 18:00:42 +0800

    使用传送门 React.Portal API

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
 import {useState} from 'react';
 import Dialog from './dialog';
+import {Button} from '../index';
 
 const DialogExample: React.FunctionComponent = () => {
   const [x, setX] = useState(true);
   return (
     <div>
       <button onClick={() => setX(!x)}>click</button>
-      <Dialog visible={x}>
-        <strong>hi11111</strong>
+      <Dialog visible={x} buttons={
+        [
+          <Button>1</Button>,
+          <Button>2</Button>,
+        ]
+      } onClose={() => {setX(false);}}>
+        <strong>hi</strong>
       </Dialog>
     </div>
   );
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,40 +1,49 @@
 import * as React from 'react';
-import {ReactNode, Fragment} from 'react';
+import {Fragment, ReactElement} from 'react';
 import './dialog.scss';
-import {Icon, Button} from '../index';
+import {Icon} from '../index';
 import {scopedClassMaker} from '../class';
+import ReactDOM from 'react-dom';
 
 type DialogProps = {
   visible: boolean;
-  children: ReactNode;
+  buttons: ReactElement[];
+  onClose: React.MouseEventHandler;
+  closeOnClickMask?: boolean;
 }
 
 const scopedClass = scopedClassMaker('xue-dialog');
 const sc = scopedClass;
 
 const Dialog: React.FunctionComponent<DialogProps> = (props) => {
-  return (
+  const onClickClose: React.MouseEventHandler = (e) => {
+    props.onClose(e);
+  };
+  const x =
     props.visible ?
       <Fragment>
         <div className={sc('mask')}>
         </div>
         <div className={sc()}>
-          <div className={sc('close')}>
+          <div className={sc('close')} onClick={onClickClose}>
             <Icon name="close"/>
           </div>
           <header className={sc('header')}>
             提示
           </header>
-          <main  className={sc('main')}>
+          <main className={sc('main')}>
             {props.children}
           </main>
           <footer className={sc('footer')}>
-            <Button>ok</Button>
-            <Button>cancel</Button>
+            {props.buttons.map((button, i) =>
+              React.cloneElement(button, {key: i})
+            )}
           </footer>
         </div>
       </Fragment>
-      : null
+      : null;
+  return (
+    ReactDOM.createPortal(x, document.body)
   );
 };
 export default Dialog;
