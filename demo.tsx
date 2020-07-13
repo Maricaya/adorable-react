6d29e2573f29e534027fcbdb51f80f9f829f3526
Maricaya <915270549@qq.com>
2020-07-13 10:41:37 +0800

    优化 demo 展示

--- demo.tsx
+++ demo.tsx
@@ -1,17 +1,36 @@
 import * as React from 'react';
+import Heiglight, {defaultProps} from 'prism-react-renderer';
+import {Button} from './lib';
+import {useState} from 'react';
 
 type Props = {
   code: string;
 }
 const Demo: React.FunctionComponent<Props> = (props) => {
+  const [codeVisible, setCodeVisible] = useState(false);
+
+  const code = <Heiglight {...defaultProps} code={props.code} language="jsx">
+    {({className, style, tokens, getLineProps, getTokenProps}) => (
+      <pre className={className} style={style}>
+            {tokens.map((line, i) => (
+              <div {...getLineProps({line, key: i})}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({token, key})}/>
+                ))}
+              </div>
+            ))}
+          </pre>
+    )}
+  </Heiglight>;
   return (
     <div>
       {props.children}
-      <pre>
-        {props.code}
-      </pre>
+      <div className="example">
+        <Button onClick={() => setCodeVisible(!codeVisible)}>查看代码</Button>
+      </div>
+      {codeVisible && code}
     </div>
-  )
-}
+  );
+};
 
 export default Demo;
