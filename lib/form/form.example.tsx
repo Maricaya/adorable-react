f0ac6be99315e1829873c3607b3646bae11eb20d
Maricaya <915270549@qq.com>
2020-07-14 17:21:33 +0800

    完成 form 受控组件

--- lib/form/form.example.tsx
+++ lib/form/form.example.tsx
@@ -1,9 +1,30 @@
 import * as React from 'react';
-import Form from './form';
+import Form, {FormValue} from './form';
+import {Fragment, useState} from 'react';
 
 const FormExample: React.FunctionComponent = () => {
+  const [formData, setFormData] = useState<FormValue>({
+    username: 'xlchu',
+    password: ''
+  });
+  const [fields] = useState([
+    {name: 'username', label: '用户名', input: {type: 'text'}},
+    {name: 'password', label: '密码', input: {type: 'password'}},
+  ]);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    console.log(formData, e);
+  };
   return (
-    <Form>123</Form>
-  )
+    <Form value={formData} fields={fields}
+          buttons={
+            <Fragment>
+              <button type="submit">提交</button>
+              <button>返回</button>
+            </Fragment>
+          }
+          onChange={(newValue) => setFormData(newValue)}
+          onSubmit={onSubmit}/>
+  );
 };
 export default FormExample;
--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -1,9 +1,39 @@
 import * as React from 'react';
 
-const Form: React.FunctionComponent = () => {
+export type FormValue = {
+  [K: string]: any
+}
+
+type Props = {
+  value: FormValue;
+  fields: Array<{ name: string, label: string, input: { type: string } }>;
+  buttons: React.ReactElement;
+  onSubmit: React.FormEventHandler<HTMLFormElement>;
+  onChange: (value: FormValue) => void;
+}
+
+const Form: React.FunctionComponent<Props> = (props) => {
+  const formData = props.value;
+  const onSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    props.onSubmit(e);
+  };
+  const onInputChange = (name: string, value: string) => {
+    const newFormValue = {...formData, [name]: value};
+    props.onChange(newFormValue);
+  };
   return (
-    <form>
-      123
+    <form onSubmit={onSubmit}>
+      {props.fields.map(f => (
+        <div key={f.name}>
+          {f.label}
+          <input type={f.input.type} value={formData[f.name]}
+                 onChange={(e) => onInputChange(f.name, e.target.value)}/>
+        </div>))
+      }
+      <div>
+        {props.buttons}
+      </div>
     </form>
   );
 };
