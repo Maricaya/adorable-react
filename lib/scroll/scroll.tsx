93997364a667e8b70805656e453bb83ff96c8c1d
Maricaya <915270549@qq.com>
2020-07-20 20:15:41 +0800

    js 隐藏滚动条

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import './scroll.scss';
 import {HTMLAttributes} from 'react';
+import {scrollbarWidth} from './scrollbar-width';
 
 type Props = {
 
@@ -10,7 +11,7 @@ const scroll: React.FunctionComponent<Props> = (props) => {
   const {children, ...rest} = props;
   return (
     <div className="xue-scroll" {...rest}>
-      <div className="xue-scroll-inner">
+      <div className="xue-scroll-inner" style={{right: -scrollbarWidth()}}>
         {children}
       </div>
     </div>
