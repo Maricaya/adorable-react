19e32effc0e55f8e476df01f0752c8523d39fc5c
Maricaya <915270549@qq.com>
2020-07-21 10:10:45 +0800

    add tree

--- lib/tree/tree.example.tsx
+++ lib/tree/tree.example.tsx
@@ -1,9 +1,32 @@
 import * as React from 'react';
 import {Tree} from '../index';
+import {useState} from 'react';
 
-const TreeExample: React.FunctionComponent = () => {
+const TreeExample: React.FC = () => {
+  const [array] = useState([
+    {
+      text: '一',
+      value: 1,
+      children: [
+        {text: '一只一', value: '1.1'},
+        {text: '一只二', value: '1.2'}
+      ]
+    },
+    {
+      text: '二',
+      value: 2,
+      children: [
+        {text: '二只一', value: '2.1'},
+        {text: '二只二', value: '2.2'}
+      ]
+    }
+  ]);
   return (
-    <Tree/>
+    <div>
+      Tree
+      <h1>展示数据</h1>
+      <Tree sourceData={array}/>
+    </div>
   );
 };
 
--- lib/tree/tree.tsx
+++ lib/tree/tree.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
 
-const Tree: React.FunctionComponent = () => {
-  return (
-    <>
+type SourceDataItem = {
+  text: string, value: string | number, children?: SourceDataItem[]
+}
 
-    </>
+type Props = {
+  sourceData: SourceDataItem[]
+}
+const Tree: React.FC<Props> = (props) => {
+  return (
+    <div>
+      {props.sourceData.map(item => {
+        return <div>
+          {item.text}
+          {item.children && item.children.map(item2 => {
+            return <div>{item2.text}</div>;})
+          }
+        </div>;
+      })}
+    </div>
   );
 };
 
